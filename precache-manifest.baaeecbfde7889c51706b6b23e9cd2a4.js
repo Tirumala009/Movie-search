37000a54ba24d0750c6bbde6ceac94e2ef472d86
Tirumala009 <69470807+Tirumala009@users.noreply.github.com>
2020-08-25 18:01:37 +0100

    Updates

---
+++ precache-manifest.baaeecbfde7889c51706b6b23e9cd2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf98594347af1cfd516d7fefa0d208f7",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "e7d864afdf88b6a5b02a",
+    "url": "/movie-search/static/css/main.f46204a3.chunk.css"
+  },
+  {
+    "revision": "5f507dfde155c8beb4a3",
+    "url": "/movie-search/static/js/2.09fd988e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.09fd988e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d864afdf88b6a5b02a",
+    "url": "/movie-search/static/js/main.79ef9fc8.chunk.js"
+  },
+  {
+    "revision": "2310a5ff34270c3ff4da",
+    "url": "/movie-search/static/js/runtime-main.c208aea9.js"
+  }
+]);
